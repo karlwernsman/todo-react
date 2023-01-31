b47f0d6c9eba9c10dac64b638bc3f5bab5e7df77
karl wernsman <wernsmank@gmail.com>
2023-01-30 17:54:34 -0800

    can complete a today - adds strike through

--- src/components/todos/TodosList.js
+++ src/components/todos/TodosList.js
@@ -1,17 +1,28 @@
-import React from 'react';
 import { useTodosContext } from '../../context/TodosContext.js';
+import { getListTodos, toggleTodoItem } from '../../services/todos.js';
+import './TodoList.css';
 
 export default function TodosList() {
-  const { todos } = useTodosContext();
+  const { todos, setTodos } = useTodosContext();
+
+  const handleComplete = async (todo) => {
+    try {
+      const updatedTodo = await toggleTodoItem(todo);
+      setTodos((prevTodo) => (prevTodo.id === todo.id ? updatedTodo : prevTodo));
+      const getTodos = await getListTodos();
+      setTodos(getTodos);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
 
   return (
     <>
       {todos.map((todo) => (
         <div key={todo.id}>
-          <label>
-            <input type="checkbox" />
+          <p onClick={() => handleComplete(todo)} className={`${todo.complete}`}>
             {todo.description}
-          </label>
+          </p>
         </div>
       ))}
     </>
--- src/services/todos.js
+++ src/services/todos.js
@@ -9,3 +9,13 @@ export async function createListTodo(description, complete) {
   const response = await client.from('todos').insert([{ description, complete }]).single();
   return checkError(response);
 }
+
+export async function toggleTodoItem({ id, complete }) {
+  const response = await client
+    .from('todos')
+    .update({ complete: !complete })
+    .match({ id })
+    .single();
+
+  return checkError(response);
+}
