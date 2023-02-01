af662d180403148f41c69aa209866d92b16d15fc
karl wernsman <wernsmank@gmail.com>
2023-01-31 16:01:12 -0800

    clear all todos button functioning

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -4,15 +4,25 @@ import { useUser } from '../../context/UserContext.js';
 import TodosForm from './TodosForm.js';
 import TodosList from './TodosList.js';
 import './Todos.css';
+import { useTodosContext } from '../../context/TodosContext.js';
 
 export default function Todos() {
-  //   const handleClear = async () => {
-  //     try {
-  // delete
-  //     } catch (e) {
-  //       console.error(e.message);
-  //     }
-  //   };
+  const { todos, setTodos } = useTodosContext();
+  // const handleClearCompleted = async () => {
+  //   try {
+  //     setTodos();
+  //   } catch (e) {
+  //     console.error(e.message);
+  //   }
+  // };
+  const handleClearAll = async () => {
+    try {
+      let todos = [];
+      setTodos(todos);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
   const { user } = useUser();
 
   if (!user) {
@@ -25,6 +35,7 @@ export default function Todos() {
         <TodosForm />
         <TodosList />
         <button>Clear completed todos</button>
+        <button onClick={handleClearAll}>Clear ALL todos</button>
       </div>
     </div>
   );
